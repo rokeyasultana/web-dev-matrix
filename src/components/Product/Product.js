127cabefaa35384f932905d9428442cc4ad25461
Rokeya Sultana <rokeyasbdgmail.comgit config --global user.name Rokeya>
2023-09-28 21:33:16 +0600

    Add heart icon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { FaRegHeart } from "react-icons/fa"; 
 
 const Product = (props) => {
   const { img, name, price, avatar, bakery } = props.product;
   return (
     <div>
       <div className="border-2 border-[#00000040] rounded-[13px] card">
-        <img src={img} alt="" />
+      <div className="relative">
+          <img src={img} alt="" className='h-[200px] w-[400px]'/>
+          <div className="absolute top-0 right-0 mt-2 mr-2">
+            <div className="rounded-full bg-white p-2">
+              <FaRegHeart size={15} color="#FF0000" /> 
+            </div>
+          </div>
+        </div>
         <div className="text-left px-3 pb-3">
           <h6 className="mt-3 font-bold">{name}</h6>
           <div className="flex gap-3 mt-3">
